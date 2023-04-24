1e24950bf444ec660e4ac550c2481fa0f57395d2
Matheus de Freitas <matheussfr@gmail.com>
2023-04-24 19:51:30 -0300

    efetuada carga de dados e apresentado o dataframe

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import pandas as pd 
 import streamlit as st 
 import numpy as np 
+
+def load_data(): 
+    return pd.read_csv("C:\\Users\\Matheus\\repos\\curso_git\\projeto\\data\\processed\\bikes_completed.csv")
+    
+df = load_data()
+
+st.dataframe(df)
+
