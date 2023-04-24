9e3a435b6163101a9d44b85917484d2a9033ea02
Matheus de Freitas <matheussfr@gmail.com>
2023-04-24 20:00:17 -0300

    Estrutura de pastas ajustadas e removidos arquivos nao utilizados

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import numpy as np
 
 def load_data(): 
     return pd.read_csv("C:\\Users\\Matheus\\repos\\curso_git\\projeto\\data\\processed\\bikes_completed.csv")
-    
-df = load_data()
 
-st.dataframe(df)
+def main(): 
+    
+    df = load_data()
+
+    st.dataframe(df)
+
+if __name__ == '__main__': 
+    main() 
 
