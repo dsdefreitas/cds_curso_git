1ce9416ab660ff74bf47fc454cafecaa81164bea
Matheus de Freitas <matheussfr@gmail.com>
2023-04-24 20:23:20 -0300

    Criado arquivo de carregamento de dados e ajustado arquivo principal para utilizar esse novo arquivo criado

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import pandas as pd 
 import streamlit as st 
-import numpy as np 
 
-def load_data(): 
-    return pd.read_csv("C:\\Users\\Matheus\\repos\\curso_git\\projeto\\data\\processed\\bikes_completed.csv")
+from src.extraction import load_data 
+
+st.set_page_config(layout= 'wide')
 
 def main(): 
     
-    df = load_data()
+    df_raw = load_data()
 
-    st.dataframe(df)
+    st.dataframe(df_raw)
 
 if __name__ == '__main__': 
     main() 
+
 
